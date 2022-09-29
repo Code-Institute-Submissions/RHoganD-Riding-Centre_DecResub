57e5810edbab0276c55d011990eb007858dba3c1
RHoganD <richpatric@hotmail.com>
2022-09-29 15:44:27 +0000

    Add media query screen size 1090

--- assets/menu.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById("menuline");
-const navlist = document.getElementById("navlistmenu");
-
-toggleButton.addEventListener('click', () =>{
-  navlist.classlist.toggle("active");
-
-})

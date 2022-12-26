cfe8f3917ec8dc56af65faaa92aace6d27faae61
RHoganD <richpatric@hotmail.com>
2022-12-26 14:52:56 +0000

    tesing speed and optimizing performance

--- asset/js/toggle.js
+++ asset/js/toggle.js
@@ -3,13 +3,6 @@ const navbarLinks = document.getElementsByClassName('navbar-menu') [0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-});
+})
 
 
-function init() {
-  var vidDefer = document.getElementsByTagName('iframe');
-  for (var i=0; i<vidDefer.length; i++) {
-  if(vidDefer[i].getAttribute('data-src')) {
-  vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-  } } }
-  window.onload = init;
